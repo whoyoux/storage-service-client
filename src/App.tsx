543996687cf98180236eb98592e1eb126f13f484
whoyoux <48690153+whoyoux@users.noreply.github.com>
2024-08-22 18:09:47 +0200

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,37 @@
-import { CircleSlash } from "lucide-react";
+import { Check, CircleAlert } from "lucide-react";
 import { Alert, AlertDescription, AlertTitle } from "./components/ui/alert";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { useState } from "react";
 import { Progress } from "./components/ui/progress";
 
+import axios from "axios";
+import { SERVER_URL } from "./lib/config";
+
+type UploadAPIResponse =
+	| {
+			success: true;
+	  }
+	| {
+			success: false;
+			message: string;
+	  };
+
 function App() {
 	const [error, setError] = useState("");
+	const [success, setSuccess] = useState("");
+
+	const [progress, setProgress] = useState(0);
+	const [isUploading, setIsUploading] = useState(false);
 
 	const [file, setFile] = useState<File | null>(null);
 	const [email, setEmail] = useState("");
 
 	const upload = async (e: React.FormEvent<HTMLFormElement>) => {
+		setProgress(0);
+
 		setError("");
+		setSuccess("");
 		e.preventDefault();
 		if (!file) {
 			setError("Please select a file to upload!");
@@ -24,7 +43,34 @@ function App() {
 			return;
 		}
 
-		//API call via axios and show progress bar
+		const formData = new FormData();
+		formData.append("file", file);
+
+		setIsUploading(true);
+		const result = await axios.post<UploadAPIResponse>(
+			`${SERVER_URL}/api/upload`,
+			formData,
+			{
+				onUploadProgress: (e) => {
+					const progress = Math.round(e.loaded / (e.total ?? 1)) * 100;
+					setProgress(progress);
+				},
+			},
+		);
+		setIsUploading(false);
+
+		if (!result.data.success) {
+			setError(result.data.message);
+			return;
+		}
+
+		if (result.data.success) {
+			setSuccess(
+				"File uploaded successfully! Check your email for the download link.",
+			);
+		} else {
+			setError("An error occurred while uploading the file!");
+		}
 	};
 
 	return (
@@ -39,16 +85,24 @@ function App() {
 			>
 				{!!error && (
 					<Alert variant="destructive">
-						<CircleSlash className="size-4" />
+						<CircleAlert className="size-4" />
 						<AlertTitle>Heads up!</AlertTitle>
 						<AlertDescription>{error}</AlertDescription>
 					</Alert>
 				)}
+
+				{!!success && (
+					<Alert>
+						<Check className="size-4" />
+						<AlertTitle>Success!</AlertTitle>
+						<AlertDescription>{success}</AlertDescription>
+					</Alert>
+				)}
+
 				<Input
 					type="file"
 					placeholder="Select a file to upload"
 					onChange={(e) => {
-						console.log(e.target.files);
 						setFile(e.target.files?.[0] ?? null);
 					}}
 				/>
@@ -63,7 +117,9 @@ function App() {
 				</p>
 				<Button type="submit">Upload</Button>
 			</form>
-			<Progress className="max-w-[500px] mt-10" value={25} />
+			{isUploading && (
+				<Progress className="max-w-[500px] mt-10" value={progress} />
+			)}
 		</main>
 	);
 }
